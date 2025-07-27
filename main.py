41ca284ee61f8cf181ee0a677145420082322304
xgeosoft <xgeosoft@gmail.com>
2025-07-27 23:52:22 +0100

    Modification de k'ui personnel et complétion de la classe FenetrePersonnel

--- main.py
+++ main.py
@@ -1,11 +1,10 @@
-from PyQt5.QtWidgets import QApplication
-from views.fenetre_principale import MenuPrincipale
 import sys
+from PyQt5.QtWidgets import QApplication, QMainWindow
+from views.fenetre_principale import FenetrePrincipale
+from PyQt5.QtWidgets import QWidget
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     app = QApplication(sys.argv)
-    fenetre = MenuPrincipale()
+    fenetre = FenetrePrincipale()
     fenetre.show()
     sys.exit(app.exec_())
-    
-    
--- views/fenetre_principale.py
+++ views/fenetre_principale.py
@@ -1,29 +1,21 @@
-# views/main_window.py
-from PyQt5.QtWidgets import QMainWindow
-from class_ui.menu_principale_ui import Ui_MenuPrincipale  # Fichier .py généré depuis Qt Designer
-from models.database_manager import DBManager
+from PyQt5.QtWidgets import QApplication, QMainWindow,QWidget
+from class_ui.fenetre_principale_ui import Ui_FenetrePrincipale
 from views.fenetre_personnel import FenetrePersonnel
-from views.fenetre_demande_conge import FenetreDemande
 
-#UI ACTUEL ET VIEUW D4AUTRE FONCTION 
 
-class MenuPrincipale(QMainWindow):
+class FenetrePrincipale(QMainWindow):
     def __init__(self):
         super().__init__()
-        DBManager.__init__(self,db_path="data/database.db")
-
-        # Charger l'interface depuis la classe générée
-        self.ui = Ui_MenuPrincipale()
+        self.ui = Ui_FenetrePrincipale()
         self.ui.setupUi(self)
-
-        # Connexion de l'action menu vers la méthode de navigation
-        self.ui.pushButton_ouvrir_personnel.clicked.connect(self.ouvrir_personnel)
-        self.ui.pushButton_ouvrir_conge.clicked.connect(self.ouvrir_demande_conge)
-
-    def ouvrir_personnel(self):
-        self.fenetrepersonnel = FenetrePersonnel()
-        self.fenetrepersonnel.show()
         
-    def ouvrir_demande_conge(self):
-        self.fenetredemandeconge = FenetreDemande()
-        self.fenetredemandeconge.show()
+        self.fenetre_personnel = FenetrePersonnel()
+        
+        self.ui.btn_information_personnel.clicked.connect(self.ouvrir_information_personnel)
+        
+    
+    def ouvrir_information_personnel(self):
+        self.setCentralWidget(self.fenetre_personnel)
+        
+    
+    
